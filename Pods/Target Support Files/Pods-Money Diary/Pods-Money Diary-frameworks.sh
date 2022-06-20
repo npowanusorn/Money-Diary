8e83f6619e0c20ddedf19b6b165c1cd5a1704523
Nucha Powanusorn <nucha.powanusorn@gmail.com>
2022-06-19 21:22:42 -0700

    added swipemenu pod and removed textview+placeholder

--- Pods/Target Support Files/Pods-Money Diary/Pods-Money Diary-frameworks.sh
+++ Pods/Target Support Files/Pods-Money Diary/Pods-Money Diary-frameworks.sh
@@ -177,11 +177,11 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/SPIndicator/SPIndicator.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/UITextView+Placeholder/UITextView_Placeholder.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwipeMenuViewController/SwipeMenuViewController.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/SPIndicator/SPIndicator.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/UITextView+Placeholder/UITextView_Placeholder.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwipeMenuViewController/SwipeMenuViewController.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- Pods/Target Support Files/SwipeMenuViewController/SwipeMenuViewController-umbrella.h
+++ Pods/Target Support Files/SwipeMenuViewController/SwipeMenuViewController-umbrella.h
@@ -10,8 +10,7 @@
 #endif
 #endif
 
-#import "UITextView+Placeholder.h"
 
-FOUNDATION_EXPORT double UITextView_PlaceholderVersionNumber;
-FOUNDATION_EXPORT const unsigned char UITextView_PlaceholderVersionString[];
+FOUNDATION_EXPORT double SwipeMenuViewControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwipeMenuViewControllerVersionString[];
 
